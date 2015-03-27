134ca57cdb4f37ab801b17c4c7098082a658fa26
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-03-27 10:46:29 +0800

    added r10k installation into prod

---
+++ site/profile/manifests/r10k_installation.pp
@@ -0,0 +1,13 @@
+class { 'r10k':
+  sources => {
+    'puppet' => {
+      'remote'  => '/vagrant/config/environmentsrepo.git',
+      'basedir' => "${::settings::confdir}/environments",
+      'prefix'  => false,
+    }
+  },
+  purgedirs         => ["${::settings::confdir}/environments"],
+  manage_modulepath => true,
+  modulepath        => "${::settings::confdir}/environments/\$environment/modules:/opt/puppet/share/puppet/modules",
+}
+
