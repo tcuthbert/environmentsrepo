2015aac7b0d2f8b7dd1601661f4ec60ccb25a675
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-03-27 15:04:14 +0800

    fixing r10k

---
+++ site/base/manifests/profiles/manifests/r10k_installation.pp
@@ -0,0 +1,16 @@
+class base::profiles::r10k_installation {
+  class { 'r10k':
+    sources => {
+      'puppet' => {
+        'remote'  => 'ssh://root@master-top.puppetdemo.local/vagrant/config/environmentsrepo',
+        'basedir' => "${::settings::confdir}/environments",
+        'prefix'  => false,
+      }
+    },
+    purgedirs         => ["${::settings::confdir}/environments"],
+    manage_modulepath => true,
+    modulepath        => "${::settings::confdir}/environments/\$environment/modules:/opt/puppet/share/puppet/modules",
+  }
+
+  class { '::r10k::mcollective': }
+}
