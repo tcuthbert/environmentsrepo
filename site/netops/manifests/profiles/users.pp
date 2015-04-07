f8fcd84ad8d0e6bf3a98467f54569960769eddfa
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-04-07 16:03:57 +0800

    Updated env
    Removed redis
    Defined user mgmt through manifests

--- site/netops/manifests/profiles/users.pp
+++ site/netops/manifests/profiles/users.pp
@@ -1,12 +1,19 @@
 class netops::profiles::users (
   $users
 ){
+  define netops::profiles::initpasswd {
+    $eof = "<<EOF\r\nP@ssw0rd1\r\nP@ssw0rd1\r\nEOF"
+    exec { "initialise ${name}'s passwd":
+      command => "passwd -f ${name} ${eof}",
+      path => ['/usr/bin', '/bin', '/sbin']
+    }
+  }
 
   $names = split($users, "\ ")
   user { $names:
     ensure     => present,
-    password   => hiera("users_tom_password"),
     groups     => ['wheel'],
     managehome => true,
-  }
+  } ->
+  netops::profiles::initpasswd {$names:}
 }
--- site/secops/manifests/profiles/users.pp
+++ site/secops/manifests/profiles/users.pp
@@ -1,3 +1,19 @@
-class secops::profiles::users {
-  users { security_admins: }
+class secops::profiles::users ( 
+  $users
+){
+  define secops::profiles::initpasswd {
+    $eof = "<<EOF\r\nP@ssw0rd1\r\nP@ssw0rd1\r\nEOF"
+    exec { "initialise ${name}'s passwd":
+      command => "passwd -f ${name} ${eof}",
+      path => ['/usr/bin', '/bin', '/sbin']
+    }
+  }
+
+  $names = split($users, "\ ")
+  user { $names:
+    ensure     => present,
+    groups     => ['wheel'],
+    managehome => true,
+  } ->
+  secops::profiles::initpasswd {$names:}
 }
