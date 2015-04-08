7b31dc9717264582c85205b9b30b620f160e7c47
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-04-08 23:36:37 +0800

    removed pe site.pp

--- site.pp
+++ site.pp
@@ -1,14 +1,3 @@
-node /master-mid/ {
-  include pe_repo::platform::el_6_x86_64
-  include pe_repo
-  class { puppet_enterprise::profile::master: 
-    enable_ca_proxy => true
-  }
-  include puppet_enterprise::profile::mcollective::peadmin
-  include puppet_enterprise::profile::master::mcollective
-  hiera_include('classes')
-}
-
 node default {
   hiera_include('classes')
 }
